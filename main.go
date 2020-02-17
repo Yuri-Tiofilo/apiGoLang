1d811e13873ebfa5bd6cae5fb388d7b2f8aa9a45
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-02-16 23:02:52 -0300

    Refactory Code

--- main.go
+++ main.go
@@ -5,10 +5,20 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Bem vindo")
-	})
+func initialRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bem vindo Two")
+}
 
+func configRoutes() {
+	http.HandleFunc("/", initialRoute)
+}
+
+func serverConfig() {
+	configRoutes()
+	fmt.Println("Servidor está rodando")
 	http.ListenAndServe(":3333", nil) //DefaultServerMux
+}
+
+func main() {
+	serverConfig()
 }
