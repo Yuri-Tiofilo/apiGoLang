ed5cffbf9d4937035a4b0cfd29a8e1cf657056d2
Yuri-Tiofilo <yusilva12345@gmail.com>
2020-02-16 23:26:12 -0300

    Create Routes ListBooks

--- main.go
+++ main.go
@@ -1,16 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type livro struct {
+	Id     int
+	Title  string
+	Author string
+}
+
+var Livros []livro = []livro{
+	livro{
+		Id:     1,
+		Title:  "A vida",
+		Author: "Yuri",
+	},
+	livro{
+		Id:     2,
+		Title:  "A vida",
+		Author: "Yuri",
+	},
+	livro{
+		Id:     3,
+		Title:  "A vida",
+		Author: "Yuri",
+	},
+	livro{
+		Id:     4,
+		Title:  "A vida",
+		Author: "Yuri",
+	},
+}
 
 func initialRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bem vindo Two")
 }
 
+func listBooks(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Livros)
+}
+
 func configRoutes() {
 	http.HandleFunc("/", initialRoute)
+	http.HandleFunc("/books", listBooks)
 }
 
 func serverConfig() {
